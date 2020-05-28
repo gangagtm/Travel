70ce4c57bafc2f6a0f2338cb0fc7049df16ee961
Ganga Gautam <gangagautam@Gangas-MBP.twlakes.net>
2020-05-27 23:23:36 -0400

    First commit

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gangagautam/Documents/OOP/Webapp with Django/second/.venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
